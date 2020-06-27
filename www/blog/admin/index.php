8533786add8617d227caa36da2c2c3939985eebb
Morten Tangerås <morten@bewide.no>
2020-06-27 19:15:05 +0200

    Added login

--- www/blog/admin/index.php
+++ www/blog/admin/index.php
@@ -21,9 +21,14 @@ if (is_object($BlogAdminObj) && $BlogAdminObj->isAuthorized()) {
 	<html>
 		<head>
 			<title>EmbedBlog Login</title>
+			<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		</head>
 		<body>
-			Login
+			<form onsubmit="return false;">
+				<input type="email" name="username" placeholder="Username">
+				<input type="passord" name="password" placeholder="Password">
+				<button data-action="signin">Sign in</button>
+			</form>
 		</body>
 	</html>
 <?php
