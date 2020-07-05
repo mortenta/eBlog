95fd39cb3278ff25e589d9abb3b6a6192f49e9e6
Morten Tangerås <morten@bewide.no>
2020-07-05 19:23:16 +0200

    og:image & og:url bug

--- www/blog/inc/head.php
+++ www/blog/inc/head.php
@@ -10,10 +10,12 @@ $hide = FALSE;
 		<meta property="og:type" content="<?php if ($type) { print $type; } else { print 'website'; } ?>" />
 		<meta property="og:title" content="<?php print $title; ?>" />
 		<meta property="og:description" content="<?php print $description; ?>" />
-<?php if ($image) { ?>		<meta property="og:image" content="<?php print $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$image; ?>" />
-<?php } ?>
-<?php if ($url_path) { ?>		<meta property="og:url" content="<?php print $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$url_path; ?>" />
-<?php } ?><?php print $head; ?>
+		<?php if ($image && strlen($image)>1) { ?>
+			<meta property="og:image" content="<?php print $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$image; ?>" />
+		<?php } ?>
+		<?php if ($url_path && strlen($url_path)>1) { ?>
+			<meta property="og:url" content="<?php print $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$url_path; ?>" />
+		<?php } ?><?php print $head; ?>
 		<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700|Handlee" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="/blog/css/screen.css" media="screen"/>
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'/>
